e051803b9c1b34ae22a1be1432aa9f7269a0e470
pgoddijn <pgoddijn@gmail.com>
2018-05-06 01:13:13 +0200

    Fixed the single quotes issue within single quotes

--- configure.sh
+++ configure.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo '#!/bin/bash
+echo $'#!/bin/bash
 PATH=/sbin:/bin
 
-sed -i ''s/"exit_type":"Crashed"/"exit_type":"Normal"/'' ~/.config/chromium/Default/Preferences
-sed -i ''s/"exited_cleanly":false/"exited_cleanly":true/'' ~/.config/chromium/Default/Preferences >> ~/.config/lxsession/LXDE-pi/chromiumsed.sh
+sed -i \'s/\"exit_type\":\"Crashed\"/\"exit_type\":\"Normal\"/\' ~/.config/chromium/Default/Preferences
+sed -i \'s/\"exited_cleanly\":false/\"exited_cleanly\":true/\' ~/.config/chromium/Default/Preferences' >> ~/.config/lxsession/LXDE-pi/chromiumsed.sh
 
 chmod 760 ~/.config/lxsession/LXDE-pi/chromiumsed.sh
 
