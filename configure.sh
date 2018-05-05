65f13e6cd6a3ba2149056270d1c468c01a102522
pgoddijn <pgoddijn@gmail.com>
2018-05-05 23:07:07 +0200

    Doubled the single quotes

--- configure.sh
+++ configure.sh
@@ -3,8 +3,8 @@
 echo '#!/bin/bash
 PATH=/sbin:/bin
 
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences' >> ~/.config/lxsession/LXDE-pi/chromiumsed.sh
+sed -i ''s/"exit_type":"Crashed"/"exit_type":"Normal"/'' ~/.config/chromium/Default/Preferences
+sed -i ''s/"exited_cleanly":false/"exited_cleanly":true/'' ~/.config/chromium/Default/Preferences >> ~/.config/lxsession/LXDE-pi/chromiumsed.sh
 
 chmod 760 ~/.config/lxsession/LXDE-pi/chromiumsed.sh
 
